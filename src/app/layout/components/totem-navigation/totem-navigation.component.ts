4ad83337028620d40fa5262c476ba3df13613216
trueOpatcher <maxx.tiechko@gmail.com>
2022-08-18 23:28:21 +0300

    web3auth

--- src/app/layout/components/totem-navigation/totem-navigation.component.ts
+++ src/app/layout/components/totem-navigation/totem-navigation.component.ts
@@ -18,16 +18,16 @@ export class TotemNavigationComponent implements OnInit {
 
   async web3Login() {
     this.loading = true;
+
     await this.web3Auth.init();
     await this.web3Auth.login();
+
     const userInfo: any = await this.web3Auth.getUserInfo();
     console.log(userInfo);
     const avatar = userInfo.profileImage;
     this.avatar = avatar;
 
     this.loading = false;
-
-
   }
 
 }
