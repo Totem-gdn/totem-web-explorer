b5eeb6f10ea3c2264c2fb7d058761e9d14457bf5
trueOpatcher <maxx.tiechko@gmail.com>
2022-08-23 14:18:30 +0300

    profile routes

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { SharedModule } from "@app/shared/shared.module";
-import { ProfileInfoComponent } from "./profile-info/profile-info.component";
+import { ProfileInfoModule } from "./profile-info/profile-info.module";
 import { ProfileComponent } from "./profile.component";
 import { ProfileRoutes } from "./profile.routing";
 
@@ -13,7 +13,7 @@ import { ProfileRoutes } from "./profile.routing";
     imports: [
         SharedModule,
         RouterModule.forChild(ProfileRoutes),
-        ProfileInfoComponent
+        ProfileInfoModule
     ]
 })
 
