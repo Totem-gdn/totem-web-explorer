d11f56aef24cccf833c34a2e9b53565f47e3b2ad
timoshchuk-web <irunarpg0@gmail.com>
2022-09-21 02:02:44 +0300

    added image reader

--- src/app/core/enums/submission-tabs.enum.ts
+++ src/app/core/enums/submission-tabs.enum.ts
@@ -3,3 +3,10 @@ export enum SUBMISSION_TABS {
   DETAILS = 'details',
   LINKS = 'links'
 }
+
+export enum DROP_BLOCK_TYPE {
+  COVER = 'cover',
+  CARD = 'card',
+  SEARCH = 'search',
+  GALLERY = 'gallery',
+}
--- src/app/modules/add-your-game/containers/details-tab/details-tab.component.ts
+++ src/app/modules/add-your-game/containers/details-tab/details-tab.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DROP_BLOCK_TYPE } from '@app/core/enums/submission-tabs.enum';
 import { UserStateService } from '@app/core/services/user-state.service';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'totem-details-tab',
@@ -13,6 +14,16 @@ import { Subscription } from 'rxjs';
 export class DetailsTabComponent implements OnInit, OnDestroy {
 
   subs: Subscription = new Subscription();
+  file!: File;
+  imageUrl!: string;
+  imageReader: FileReader = new FileReader();
+
+  imageHover: any = {
+    coverImgHovered: false,
+    cardImgHovered: false,
+    searchImgHovered: false,
+    galleryImgHovered: false,
+  };
 
   constructor(private userStateService: UserStateService) {
   }
@@ -27,6 +38,44 @@ export class DetailsTabComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subs.unsubscribe();
+  }
+
+  setHover(type: string) {
+    if (type == DROP_BLOCK_TYPE.COVER) {
+      if (!this.imageHover.coverImgHovered) this.imageHover.coverImgHovered = true;
+    }
+    if (type == DROP_BLOCK_TYPE.CARD) {
+      if (!this.imageHover.cardImgHovered) this.imageHover.cardImgHovered = true;
+    }
+    if (type == DROP_BLOCK_TYPE.SEARCH) {
+      if (!this.imageHover.searchImgHovered) this.imageHover.searchImgHovered = true;
+    }
+    if (type == DROP_BLOCK_TYPE.GALLERY) {
+      if (!this.imageHover.galleryImgHovered) this.imageHover.galleryImgHovered = true;
+    }
+  }
+
+  removeHover(type: string) {
+    if (type == DROP_BLOCK_TYPE.COVER) {
+      this.imageHover.coverImgHovered = false;
+    }
+    if (type == DROP_BLOCK_TYPE.CARD) {
+      this.imageHover.cardImgHovered = false;
+    }
+    if (type == DROP_BLOCK_TYPE.SEARCH) {
+      this.imageHover.searchImgHovered = false;
+    }
+    if (type == DROP_BLOCK_TYPE.GALLERY) {
+      this.imageHover.galleryImgHovered = false;
+    }
+  }
+
+  getFile(event: any) {
+    console.log(event);
+    this.file = event.target.files[0];
+    console.log(this.file);
+    this.imageReader.readAsDataURL(this.file);
+    this.imageReader.onload = (event: any) => { this.imageUrl = event.target.result };
   }
 
 }
