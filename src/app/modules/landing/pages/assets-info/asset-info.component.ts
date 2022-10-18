f6dd741d024ee142341cf6f372785b9dc7f0f5e5
Denis.S <denis.s@MacBook-Pro-3.local>
2022-10-18 15:50:23 +0300

    connect DNA parser

--- src/app/modules/landing/pages/assets-info/asset-info.component.ts
+++ src/app/modules/landing/pages/assets-info/asset-info.component.ts
@@ -5,7 +5,7 @@ import { PaymentService } from "@app/core/services/crypto/payment.service";
 import { TotemItemsService } from "@app/core/services/totem-items.service";
 import { BehaviorSubject, Subject, Subscription, takeUntil } from "rxjs";
 import { Web3AuthService } from "@app/core/web3auth/web3auth.service";
-const DNA = require('dna-parser');
+const { DNAParser, ContractHandler } = require('totem-dna-parser');
 // import * as DNA from 'dna-parser'
 
 
@@ -32,7 +32,8 @@ export class AssetInfoComponent {
         // if(!item) return;
         this._item = item;
         if(!item) return;
-        item.rarity = new DNA().getItemRarity(item?.tokenId);
+        // item.rarity = new DNAParser().getItemRarity(item?.tokenId);
+       this.processItem(item?.tokenId, this.type)
     }
 
     _item!: any;
@@ -50,10 +51,11 @@ export class AssetInfoComponent {
     ngOnInit() {
         this.sliderItems();
         // this.alchService.tokenTransactionsById(this.item?.tokenId);
+        // contractHandler.getDNA()
 
-        if (this.type == 'item') this.properties = [{ title: 'Type', value: '--', tooltip: false }, { title: 'Damage', value: '--', tooltip: false }, { title: 'Range', value: '--', tooltip: false }, { title: 'Power', value: '--', tooltip: false }, { title: 'Magical Power', value: '--', tooltip: false }, { title: 'Weapon Material', value: '--', tooltip: false }, { title: 'Primary Color', value: '--', tooltip: false },]
+        if (this.type == 'item') this.properties = [{ title: 'Type', id: 'classical_element', value: '--', tooltip: false }, { title: 'Damage', id: 'damage_nd', value: '--', tooltip: false }, { title: 'Range', id: 'range_nd', value: '--', tooltip: false }, { title: 'Power', id: 'power_nd', value: '--', tooltip: false }, { title: 'Magical Power', id: 'magical_exp', value: '--', tooltip: false }, { title: 'Weapon Material', id: 'weapon_material', value: '--', tooltip: false }, { title: 'Primary Color', id: 'primary_color', value: '--', tooltip: false },]
         if (this.type == 'gem') this.properties = [{ title: 'Type', value: '--', tooltip: false }, { title: 'Damage', value: '--', tooltip: false }, { title: 'Range', value: '--', tooltip: false }, { title: 'Power', value: '--', tooltip: false }, { title: 'Magical Power', value: '--', tooltip: false }, { title: 'Weapon Material', value: '--', tooltip: false }, { title: 'Primary Color', value: '--', tooltip: false },]
-        if (this.type == 'avatar') this.properties = [{ title: 'Sex', value: '--', tooltip: false }, { title: 'Body Strength', value: '--', tooltip: false }, { title: 'Body Type', value: '--', tooltip: false }, { title: 'Skin Color', value: '--', tooltip: false }, { title: 'Hair Color', value: '--', tooltip: false }, { title: 'Eye Color', value: '--', tooltip: false }, { title: 'Hair Style', value: '--', tooltip: false }, { title: 'Weapon Type', value: '--', tooltip: false }, { title: 'Weapon Material', value: '--', tooltip: false }, { title: 'Primary Color', value: '--', tooltip: false },]
+        if (this.type == 'avatar') this.properties = [{ title: 'Sex', id: 'sex_bio', value: '--', tooltip: false }, { title: 'Body Strength', id: 'body_strength', value: '--', tooltip: false }, { title: 'Body Type', id: 'body_type', value: '--', tooltip: false }, { title: 'Skin Color', id: 'human_skin_color', value: '--', tooltip: false }, { title: 'Hair Color', id: 'human_hair_color', value: '--', tooltip: false }, { title: 'Eye Color', id: 'human_eye_color', value: '--', tooltip: false }, { title: 'Hair Style', id: 'hair_styles', value: '--', tooltip: false }, { title: 'Weapon Type', id: 'weapon_type', value: '--', tooltip: false }, { title: 'Weapon Material', id: 'weapon_material', value: '--', tooltip: false }, { title: 'Primary Color', id: 'primary_color', value: '--', tooltip: false },]
 
     }
 
@@ -77,6 +79,33 @@ export class AssetInfoComponent {
                     this.assets = assets;
                 })
         }
+    }
+
+    async processItem (id: number, type: string) {
+        const url = 'https://matic-mumbai.chainstacklabs.com'
+        let contract = ''
+        let json = ''
+        if (this.type === 'item') {
+            contract = '0xfC5654489b23379ebE98BaF37ae7017130B45086'
+            json = DNAParser.defaultItemJson
+        } else if (this.type === 'gem') {
+            contract = '0x0e2a085063e15FEce084801C6806F3aE7eaDfBf5'
+            json = DNAParser.defaultGemJson
+        } else if (this.type === 'avatar') {
+            contract = '0xEE7ff88E92F2207dBC19d89C1C9eD3F385513b35'
+            json = DNAParser.defaultAvatarJson
+        }
+        const contractHandler = new ContractHandler(url, contract)
+
+        const DNA = await contractHandler.getDNA(id)
+        
+        const parser = new DNAParser(json, DNA)
+
+        this._item.rarity = parser.getItemRarity(id)
+
+        this.properties.map((prop) => {
+            prop.value = parser.getField(prop.id)
+        })
     }
 
     ngOnDestroy() {
