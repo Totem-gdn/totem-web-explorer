c7dbc45bc7f4aea45257d912735e8a07b8de880f
timoshchuk-web <irunarpg0@gmail.com>
2022-10-26 00:44:14 +0300

    fixed admin panel + guards

---
+++ src/app/modules/landing/pages/admin/admin.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { AdminComponent } from "./admin.component";
+
+
+export const AdminRoutes: Route[] =[
+    {
+        path: '', component: AdminComponent
+    }
+]
