e317590ebb4deddf21335f42417258ec3666e821
Maxx Tiyechko <maxx.tiechko@gmail.com>
2022-11-02 16:29:46 +0200

    increase budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   WEB3AUTH_ID: 'BMUaCGWAqX7ulSauE1dvkhhiGy1OUcVPaDNexeWCj8K9Hs4EFtOMGjhFGMnwxLOypcA4g6UlzAa8UF35POXQFtI',
-  ALCHEMY_KEY: 'https://polygon-mumbai.g.alchemy.com/v2/VUUTlTODWEJGbHRQ55qIGovGX_FUOa5L',
   TOTEM_BASE_API_URL: 'https://api.totem-explorer.com',
   TOTEM_FAUCET_API_URL: 'https://faucet.totem-explorer.com',
   ASSET_RENDERER_URL: 'https://asset-renderer.totem-explorer.com',
