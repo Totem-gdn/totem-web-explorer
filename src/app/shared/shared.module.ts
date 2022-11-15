8cd82e2beccd16f8f461de4c8a3f29dc45710eca
Roman Kostiv <roman120487@ukr.net>
2022-11-15 15:51:35 +0200

    implement and setting translate module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { NgxMaskModule } from "ngx-mask";
 import { DirectivesModule } from "./directives/directives.module";
 import { LoadingSpinner } from "./loading-spinner/locading-spinner.component";
 import { PipesModule } from "./pipes/pipes.module";
 import { TotemSpinnerModule } from "./totem-spinner/totem-spinner.module";
 
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [
@@ -20,11 +25,20 @@ import { TotemSpinnerModule } from "./totem-spinner/totem-spinner.module";
         PipesModule,
         DirectivesModule,
         TotemSpinnerModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            },
+            defaultLanguage: 'en'
+        })
     ],
     exports: [
         CommonModule,
         HttpClientModule,
         NgxMaskModule,
+        TranslateModule,
 
         PipesModule,
         DirectivesModule,
