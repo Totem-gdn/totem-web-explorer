490ce4025ba0040e110160d8f00319e746d31b4e
timoshchuk-web <irunarpg0@gmail.com>
2022-12-08 19:05:37 +0200

    sw update

---
+++ src/app/service-worker.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
+
+@Injectable({providedIn: 'root'})
+
+export class ServiceWorkerService {
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  //Here you can check the versions
+  listenNewVersion() {
+    this.swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+        switch (event.type) {
+          case 'VERSION_DETECTED':
+            console.log(`Downloading new app version: ${event.version.hash}`);
+            if (confirm('Software update avaialble.')) {
+              this.swUpdate.activateUpdate().then(() => {
+                document.location.reload();
+              });
+            }
+            break;
+          case 'VERSION_READY':
+            console.log(`Current app version: ${event.currentVersion.hash}`);
+            console.log(`New app version ready for use: ${event.latestVersion.hash}`);
+            break;
+          case 'VERSION_INSTALLATION_FAILED':
+            console.log(`Failed to install app version '${event.version.hash}': ${event.error}`);
+            break;
+        }
+    });
+  }
+}
