5739dbba42a0f84def5aab5da70b9858323ae215
timoshchuk-web <irunarpg0@gmail.com>
2022-12-12 15:28:52 +0200

    version dialog implemented

---
+++ src/app/core/dialogs/version-dialog/version-dialog.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from "@angular/core";
+import { SharedModule } from "@app/shared/shared.module";
+import { VersionDialogComponent } from "./version-dialog.component";
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatButtonModule } from "@angular/material/button";
+import { ImageCropperModule } from "ngx-image-cropper";
+import { MatIconModule } from "@angular/material/icon";
+import { MatRippleModule } from "@angular/material/core";
+import { TotemButtonModule } from "@app/components/utils/totem-button/totem-button.module";
+import { TotemSpinnerModule } from "@app/shared/totem-spinner/totem-spinner.module";
+import { BackgroundCircleModule } from "@app/components/utils/bg-circle/bg-circle.module";
+
+@NgModule({
+    declarations: [
+      VersionDialogComponent,
+    ],
+    imports: [
+        SharedModule,
+        FlexLayoutModule,
+        MatProgressBarModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatRippleModule,
+        ImageCropperModule,
+        TotemSpinnerModule,
+        MatIconModule,
+        MatRippleModule,
+        TotemButtonModule,
+        BackgroundCircleModule,
+    ],
+    exports: [
+      VersionDialogComponent
+    ]
+})
+
+export class VersionDialogModule {
+
+}
