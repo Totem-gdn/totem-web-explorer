2600047599556cc9ae9685dca5fcc758b8be1dcd
Roman Kostiv <roman120487@ukr.net>
2022-12-20 23:15:32 +0200

    decode and popup with base64

--- src/app/modules/landing/pages/assets-info/components/legacy/item-legacy.component.ts
+++ src/app/modules/landing/pages/assets-info/components/legacy/item-legacy.component.ts
@@ -1,23 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SnackNotifierService } from '@app/components/utils/snack-bar-notifier/snack-bar-notifier.service';
 import { AssetInfo } from '@app/core/models/interfaces/asset-info.model';
-import { Achievement } from '@app/core/models/interfaces/legacy.model';
+import { Achievement, Legacy } from '@app/core/models/interfaces/legacy.model';
 import { LegacyService } from '@app/core/services/crypto/legacy.service';
 import { OnDestroyMixin, untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
 
 @Component({
   selector: 'item-legacy',
   templateUrl: './item-legacy.component.html',
-  styleUrls: ['./item-legacy.component.scss']
+  styleUrls: ['./item-legacy.component.scss'],
 })
 export class ItemLegacyComponent extends OnDestroyMixin implements OnInit {
-
-  achievementData(data: string) {
-    if (data.length > 4) {
-      return data.slice(0, 4) + '...' + data.slice(-4);
-    }
-    return data;
-  }
+  prevIndexSelected: number = 0;
+  achievements!: Achievement[];
+  @Input() asset!: AssetInfo;
 
   constructor(
     private legacyService: LegacyService,
@@ -26,20 +22,34 @@ export class ItemLegacyComponent extends OnDestroyMixin implements OnInit {
     super();
   }
 
-  achievements!: Achievement[];
-  @Input() asset!: AssetInfo;
-
   ngOnInit(): void {
     this.legacyService.fetchLegacies(this.asset.tokenId).pipe(
       untilComponentDestroyed(this),
-    ).subscribe(leg => {
+    ).subscribe((leg: Legacy) => {
       if (!leg.achievements) return;
       this.achievements = leg.achievements;
     })
   }
 
-  onCopy() {
+  onCopy(): void {
     this.messageService.open('Copied to the clipboard');
+  }
+
+  onCheckBase64(data: string, index: number): void {
+    const base64regExp: RegExp = /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$/gm;
+
+    if (base64regExp.test(data)) {
+      this.achievements[this.prevIndexSelected].base64Encoded = undefined;
+      this.achievements[index].base64Encoded = Buffer.from(data, 'base64').toString('binary');
+      this.prevIndexSelected = index;
+    }
+  }
+
+  achievementData(data: string) {
+    if (data.length > 4) {
+      return data.slice(0, 4) + '...' + data.slice(-4);
+    }
+    return data;
   }
 
 }
