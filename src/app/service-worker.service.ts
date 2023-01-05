87555024ae968b8495effb47f7ad684d134a4822
timoshchuk-web <irunarpg0@gmail.com>
2023-01-05 18:00:30 +0200

    sw logs

--- src/app/service-worker.service.ts
+++ src/app/service-worker.service.ts
@@ -16,8 +16,11 @@ export class ServiceWorkerService {
     this.swUpdate.versionUpdates.subscribe((event) => {
         switch (event.type) {
           case 'VERSION_DETECTED':
+            console.log('New ver. detected');
             break;
           case 'VERSION_READY':
+            console.log('Current ver: ', event.currentVersion);
+            console.log('New ver. ready to be installed: ', event.latestVersion);
             this.versionDialogService.openVersionDialog().subscribe((data: boolean) => {
               if (data == true) {
                 this.swUpdate.activateUpdate().then(() => {
