8536b8a05c38c69dfbe3e6e2ef80b5e558df95a4
Maxx Tiyechko <maxx.tiechko@gmail.com>
2023-01-05 17:40:51 +0200

    font update

--- src/app/components/utils/arrows/arrow-right.component.ts
+++ src/app/components/utils/arrows/arrow-right.component.ts
@@ -5,7 +5,6 @@ import { Component, Input } from "@angular/core";
     selector: 'arrow-right',
     template: `
     <div class="flex w-50px h-50px cursor-pointer bg-grey hover:bg-yellow text-light-grey hover:text-black rounded-full transition duration-200">
-                    <!-- <mat-icon class="!w-3 m-auto" [svgIcon]="'line:arrow_right'"></mat-icon> -->
                     <div class="line-arrow_right !w-3 m-auto"></div>
     </div>
     `,
